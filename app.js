56551eb3e334e4ff722f505ef179c8555081e3a5
Allen <allenvs@mail.ru>
2020-08-15 18:49:22 +0300

    cleaned some trash

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 function reverseArrayInPlace(array) {
     for (let i = 0; i < Math.floor(array.length/2); i++) {
         [array[i], array[array.length-1-i]] = [array[array.length-1-i], array[i]];
-        console.log (array[i], array[array.length-1-i]);
     }
     return(array)
 }
-
 let arrayValue = [1, 2, 3, 4, 5];
 reverseArrayInPlace(arrayValue);
 console.log(arrayValue);
